33b99850de8de41278a8bbe6c9e4af2a85687ed3
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-24 11:35:52 -0300

    ChallengesContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/global.css'
 
+import { ChallengesProvider } from "../contexts/ChallengesContext";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
 }
 
 export default MyApp
+
+/**
+ *  Todos os elementos dentro do Provider vão ter acesso a todos os dados que eu armazenar dentro daquele contexto
+ */
