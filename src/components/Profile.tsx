a96aa0ae6eb257f500d0ed7ed6e313a321191ed9
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-02-25 09:49:53 -0300

    CountdownContext created

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={ styles.profileContainer }>
       <img src="https://github.com/Lukinhasssss.png" alt="Lucas Monteiro"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Lucas Monteiro</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
